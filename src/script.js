9439c154e3f971b9b3b8268713a07e9ad71c91fa
Iggy159 <georgebazhenov27@gmail.com>
2022-05-27 02:38:31 +0300

    cloud

--- src/script.js
+++ src/script.js
@@ -1,16 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
 //Colors
 
-var Colors = {
+let Colors = {
 	red:0xf99341,
 	white:0xd8d0d1,
 	brown:0x59332e,
 	pink:0xF5986E,
 	brownDark:0x23190f,
-	blue:0x68c3c0,
+	blue:0x008000,
 }
 
 let fieldOfView, aspectRatio, nearPlane, farPlane, container, renderer;
@@ -34,7 +33,7 @@ document.addEventListener('mousemove', handleMouseMove, false)
 
 // Scene
 const scene = new THREE.Scene()
-
+scene.background = new THREE.Color( 0xb3e6ff );
 /**
  * Sizes
  */
@@ -95,7 +94,7 @@ controls.enableDamping = true
   const matLand = new THREE.MeshPhongMaterial({
     color:Colors.blue,
   	transparent:true,
-  	opacity:.6,
+  	//opacity:.6,
   	shading:THREE.FlatShading,
   })
   const meshLand = new THREE.Mesh(geomLand, matLand)
@@ -105,67 +104,118 @@ controls.enableDamping = true
 
 
 // //Cloud
-// function Cloud() {
-//   const meshCloud = new THREE.Object3D();
-//   const geomCloud = new THREE.BoxGeometry(20,20,20);
 //
-// 	const matCloud = new THREE.MeshPhongMaterial({
-// 		color:Colors.white,
-// 	});
-//
-// 	let nBlocs = 3 + Math.floor(Math.random() * 3);
-// 	for (let i = 0; i < nBlocs; i++ ){
-//
-// 	const m = new THREE.Mesh(geomCloud, matCloud);
-//
-// 		m.position.x = i * 15;
-// 		m.position.y = Math.random() * 10;
-// 		m.position.z = Math.random() * 10;
-// 		m.rotation.z = Math.random() * Math.PI * 2;
-// 		m.rotation.y = Math.random() * Math.PI * 2;
-//
-// 		let s = .1 + Math.random() * .9;
-// 		m.scale.set(s, s, s);
-//
-// 		m.castShadow = true;
-// 		m.receiveShadow = true;
-//
-// 		meshCloud.add(m);
-// 	}
-// 	scene.add(meshCloud)
-// }
-//
-//
+let Cloud = function(){
+  this.mesh = new THREE.Object3D();
+  this.mesh.name = "cloud";
+  var geom = new THREE.BoxGeometry(20,20,20);
+  var mat = new THREE.MeshPhongMaterial({
+    color:Colors.white,
+
+  });
+
+  //*
+  var nBlocs = 3+Math.floor(Math.random()*3);
+  for (var i=0; i<nBlocs; i++ ){
+    var m = new THREE.Mesh(geom.clone(), mat);
+    m.position.x = i*15;
+    m.position.y = Math.random()*10;
+    m.position.z = Math.random()*10;
+    m.rotation.z = Math.random()*Math.PI*2;
+    m.rotation.y = Math.random()*Math.PI*2;
+    var s = .1 + Math.random()*.9;
+    m.scale.set(s,s,s);
+    this.mesh.add(m);
+    m.castShadow = true;
+    m.receiveShadow = true;
+
+  }
+  //*/
+}
+
+Cloud.prototype.rotate = function(){
+  var l = this.mesh.children.length;
+  for(var i=0; i<l; i++){
+    var m = this.mesh.children[i];
+    m.rotation.z+= Math.random()*.005*(i+1);
+    m.rotation.y+= Math.random()*.002*(i+1);
+  }
+}
+
+let Sky = function(){
+  this.mesh = new THREE.Object3D();
+  this.nClouds = 20;
+  this.clouds = [];
+  var stepAngle = Math.PI*2 / this.nClouds;
+  for(var i=0; i<this.nClouds; i++){
+    var c = new Cloud();
+    this.clouds.push(c);
+    var a = stepAngle*i;
+    let h = 750 + Math.random()*200
+    c.mesh.position.y = Math.sin(a)*h;
+    c.mesh.position.x = Math.cos(a)*h;
+    c.mesh.position.z = -300-Math.random()*500;
+    c.mesh.rotation.z = a + Math.PI/2;
+    var s = 1+Math.random()*2;
+    c.mesh.scale.set(s,s,s);
+    this.mesh.add(c.mesh);
+  }
+}
+
+Sky.prototype.moveClouds = function(){
+  for(var i=0; i<this.nClouds; i++){
+    var c = this.clouds[i];
+    c.rotate();
+  }
+  this.mesh.rotation.z += game.speed*deltaTime;
+
+}
+
+let sky
+
+function createSky(){
+  sky = new Sky();
+  sky.mesh.position.y = -700
+  scene.add(sky.mesh);
+}
+createSky()
 // //Sky
 //
-// function Sky() {
-//   const meshSky = new THREE.Object3D();
-//   const nClouds = 5
-//   let stepAngle = Math.PI*2 / nClouds
 //
-//   for(let i = 0; i < nClouds; i ++) {
-//     let c = new Cloud()
-//     let a = stepAngle * i;
-// 		let h = 750 + Math.random() * 200;
+// const meshSky = new THREE.Object3D
+// const nClouds = 20
+// let stepAngle = Math.PI*2 / nClouds
+// 	//
+// for(let i = 0; i < nClouds; i ++) {
+// 	// const geomCloud = new THREE.BoxGeometry(20,20,20);
+// 	// const matCloud = new THREE.MeshPhongMaterial({
+// 	// 	color:Colors.white,
+// 	// });
 //
-// 		meshSky.position.y = Math.sin(a)*h;
-// 		meshSky.position.x = Math.cos(a)*h;
 //
-// 		meshSky.rotation.z = a + Math.PI/2;
-// 		meshSky.position.z = -400-Math.random()*400;
+// 	const m = meshCloud
 //
-// 		var s = 1+Math.random()*2;
-// 		meshSky.scale.set(s,s,s);
-// 		meshSky.add(c);
-//   }
-//   meshSky.position.y = -600
-//   scene.add(meshSky)
+//
+//   let a = stepAngle * i;
+// 	let h = 750 + Math.random() * 200;
+// 		const meshSky = new THREE.Object3D
+// 		m.meshSky.position.y = Math.sin(a)*h;
+// 		meshCloud.position.x = Math.cos(a)*h;
+//
+// 		meshCloud.rotation.z = a + Math.PI/2;
+// 		meshCloud.position.z = -400-Math.random()*400;
+//
+// 		let s = 1+Math.random()*2;
+// 		meshCloud.scale.set(s,s,s);
+// 		meshCloud.position.y = -700
+// 		meshSky.add(meshCloud);
 // }
-// Sky()
+//
+// scene.add(meshSky)
 
 
 
-	const meshAirplane = new THREE.Object3D();
+ const meshAirplane = new THREE.Object3D();
 
 	// Create the cabin
 	const geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
@@ -219,7 +269,7 @@ controls.enableDamping = true
 	propeller.add(blade);
 	propeller.position.set(50,0,0);
 	meshAirplane.add(propeller);
-	scene.add(cockpit, engine, tailPlane, sideWing, propeller)
+//	scene.add(cockpit, engine, tailPlane, sideWing, propeller)
 
 //Lights
 
@@ -251,6 +301,7 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true;
 
+
 /**
  * Animate
  */
@@ -275,7 +326,8 @@ const tick = () =>
     const deltaTime = elapsedTime - lastElapsedTime
     lastElapsedTime = elapsedTime
 		//propeller.rotation.x += 0.3
-		meshLand.rotation.z += 0.005
+		meshLand.rotation.z += 0.003
+		sky.mesh.rotation.z += .001;
 
     // Update controls
     controls.update()
